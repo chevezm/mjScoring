bc1ff126792d5cae0093e0b8ceba581fb9a77bd3
chevezm <61294146+chevezm@users.noreply.github.com>
2024-02-10 23:07:04 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,7 @@ function calculate(){
     document.getElementById('resetButton').style.visibility = 'visible';
     document.getElementById('submitButton').style.visibility = 'hidden';
 
-    setValues('name1','p1','name2','p2');
-    
-    document.getElementById('name4').innerHTML = document.getElementById('p4').value;
-
+    setValues('name1','p1','name2','p2','name3','p3','name4','p4');
     
     return;
 }
@@ -20,6 +17,8 @@ function reset(){
     
     return;
 }
+
+
 
 function setValues(...args){
     for (let i=0; i < arguments.length; i +=2) document.getElementById(arguments[i]).innerHTML = document.getElementById(arguments[i+1]).value;
