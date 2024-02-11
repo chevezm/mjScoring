af5731053b540072ba47625c4cd4203c64ecb511
chevezm <61294146+chevezm@users.noreply.github.com>
2024-02-10 23:13:41 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -9,16 +9,19 @@ function calculate(){
 }
 
 function reset(){
-    document.getElementById('finalScore').style.visibility = 'hidden';
-    document.getElementById('resetButton').style.visibility = 'hidden';
-    document.getElementById('submitButton').style.visibility = 'visible';
+
+    toggleVisibility('finalScore','resetButton',submitButton');
 
     resetValues('raw1','raw2','raw3','raw4','chip1','chip2','chip3','chip4');
     
     return;
 }
 
-
+function toggleVisibility(...args){
+    for (let arg of args){
+        document.getElementById(arg).style.visibility = document.getElementById(arg).style.visibility == 'visible' ? 'hidden' : 'visible';
+    }
+}
 
 function setValues(...args){
     for (let i=0; i < arguments.length; i +=2) document.getElementById(arguments[i]).innerHTML = document.getElementById(arguments[i+1]).value;
